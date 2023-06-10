85e447d34fdb98509d7f7dc146b1934522da0a34
amir <khazaeli7amir@gmail.com>
2023-06-10 21:07:29 +0330

    Create HomePage Route

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import Comments from "./Components/Comments/Comments";
+import Home from "./Components/Home/Home";
 import Offs from "./Components/Offs/Offs";
 import Order from "./Components/Order/Order";
 import Product from "./Components/Product/Product";
 import Users from "./Components/Users/Users";
 
 export const routes = [
+    {path: '/' , element: <Home />},
     {path: '/product' , element:<Product />},
     {path: '/comments' , element:<Comments />},
     {path: '/users' , element:<Users />},
